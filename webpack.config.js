902a97446fa921c27db86a42b4c1fb7a51afcb7f
Anna-Makovska <brain.lady404@gmail.com>
2025-11-06 18:05:47 +0200

    build project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'custom.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'custom.css',
+    }),
+  ],
+  mode: 'development',
+};
